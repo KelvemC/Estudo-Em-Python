0876fa8cdfc64dc1c3468aad81cbfb13e1db0d68
KelvemC <kelvemcif@gmail.com>
2022-05-03 20:17:45 -0300

    Arquivos Operadores e Tipo Boolean adicionados

---
+++ Curso Python/Estruturas_Lógicas_E_Condicionais/OperadoresAritmético.py
@@ -0,0 +1,35 @@
+"""
+Operadores aritméticos
+
+"""
+
+nome = 8
+x = 5    
+y = 5
+
+# Operador de atribuição = 
+
+"""
+Operador da adição +
+Operador da subtração - 
+Operador da multiplicação *
+Operador da divisão /
+"""
+x = 3
+soma = x+y
+subtracao = x-y
+multiplicacao = x * y
+divisao = x/y
+
+print("A soma de x + y =", soma)
+print("A subtração de x - y =", subtracao)
+print("A multiplicação de x * y =", multiplicacao)
+print("A divisão de x / y =", divisao)
+
+"""
+
+Operador de módulo/resto %
+Operador da exponenciação **
+Operador da divisão inteira //
+
+"""
---
+++ Curso Python/Estruturas_Lógicas_E_Condicionais/TipoBoolean.py
@@ -0,0 +1,20 @@
+"""
+Operadores Lógicos e o tipo boolean
+
+Em ciência da computação, boolean, ou lógico,
+é um tipo de dado primitivo que possui dois valores,
+que podem ser considerados como (0 ou 1), falso ou verdadeiro.
+
+É chamado boolean em homenagem a George boole
+"""
+
+# Tipo boolean : True e False
+
+"""
+Operadores lógicos
+
+and (e)
+or (ou)
+not (negação)
+"""
+
