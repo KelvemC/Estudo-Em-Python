02e11586070a7759d5feff60e5f9c7cccaaa068f
KelvemC <kelvemcif@gmail.com>
2022-05-15 13:09:08 -0300

    while, break e else, for e range foram add

---
+++ Curso Python/EstruturaRepeticao/for_python.py
@@ -0,0 +1,14 @@
+"""
+Estruturas de repetição
+
+while
+for
+do while* não existe em python
+
+s = 'pernambuco'
+for x in s:
+    print(x)
+"""
+
+for x in range(0, 101, 5): #for(x = 0; x<=100; x+=5);
+    print(x)
---
+++ Curso Python/EstruturaRepeticao/while_python.py
@@ -0,0 +1,22 @@
+"""
+Estruturas de repetição
+
+while
+for
+do while* não existe em python
+
+
+
+a = 0
+x = 0
+print(a)
+a = a + 1
+print(a)
+"""
+a = 0
+while(a<=5):
+    print(a)
+    a = a + 1   
+else:
+    print(f"{a} não é <= 5")
+
